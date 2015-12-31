fbbc48fe9c7eada170ce5af0812f50a23d93e60b
Chengguang Zhao <chengguangzhao@cheng.wireless.yale.internal>
2015-12-30 22:50:48 -0500

    Initial Commit

---
+++ LearnSwift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  LearnSwift
+//
+//  Created by Chengguang Zhao on 12/30/15.
+//  Copyright © 2015 Chengguang Zhao. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
